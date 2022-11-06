77a8f83ee2d742a710e2ff5fcf57a9ad3babce3f
Lauren <guerralauren44@gmail.com>
2022-11-06 11:05:27 +0000

    creando test

---
+++ src/test.py
@@ -0,0 +1,18 @@
+import vector_model as vm
+
+a=vm.get_terms_frequency(terms=['requerimientos','clase','class','trabajo'], 
+                         documents=['./src/script.txt','./src/script2.txt'])
+                        # documents=['./docs/Search Engine Optimization.docx','./docs/Seminario2(C++11,C++14).pdf','./docs/Seminario1(C++98).pdf',
+                        # './docs/Seminario4(Golang).pdf'])
+print(a)
+
+
+
+
+
+
+
+            
+            # items = open("script.txt", 'r')
+            # lists = items.read().split("\n")
+            # lists = [item.split() for item in lists]
