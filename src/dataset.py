a08b624548e530453cc21e6938cafd9f2edc1954
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-11-15 19:52:30 -0500

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -10,12 +10,12 @@ class Datasets:
 
     def get_dataset(self, dataset:str):
         self.dataset:CranfieldDocs = ir_datasets.load(dataset)
-        for doc in self.dataset:
+        for doc in self.dataset.docs_iter():
             self.documents.add(doc.doc_id)
         self.docslen= self.dataset.docs_count()
 
     def get_docs_data(self):
-        for data in self.dataset.docs_iter:
+        for data in self.dataset.docs_iter():
             data:CranfieldDoc= data
             yield {'id':data.doc_id, 'text':data.text}
 
