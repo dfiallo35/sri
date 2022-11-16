e78b0deb04d974a205d606164d2630446458e6cd
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-11-16 17:04:04 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from vector_model import *
 
 a = VectorModel()
 print(
-        a.run(query='what are the structural and aeroelastic problems associated with flight of high speed aircraft',
+        a.run(query='',
                 dataset='cranfield')
 )
