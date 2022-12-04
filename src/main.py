3f7561b76a712c7f3d69a0f7f3b6b5bb3ceec76c
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-04 12:15:07 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from vector_model import *
 
 a = VectorModel()
 print(
-        a.run(query='',
+        a.run(query='what similarity laws must be obeyed when constructing aeroelastic models of heated high speed aircraft',
                 dataset='cranfield')
 )
