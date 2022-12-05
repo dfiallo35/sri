e0c732d5bc8c6a6e88059630548dad9423800bf4
Paula <62489004+prp97@users.noreply.github.com>
2022-12-04 21:07:42 -0500

    + run, compare datasets, docs data, find, clean query data, rankig, umbral como en el vectorial

--- src/probabilistic_model.py
+++ src/probabilistic_model.py
@@ -1,11 +1,10 @@
 from base_model import Model
-from utils import *
 from dataset import Datasets
 from stopwords import Stopwords
 import numpy as np
 
 
-class Probabilistic_model(Model):
+class Probabilistic_Model(Model):
 
     def __init__(self):
         super().__init__()
@@ -21,6 +20,104 @@ class Probabilistic_model(Model):
 
     	# dictonary with the similarity between the query and documents
         self.query_doc_sim = dict()
+
+        # list of stopwords
+        self.stopwords = Stopwords('english')
+
+        self.dataset = Datasets()
+
+
+    def run(self, query: str, dataset: str, limit: int=None, umbral: float=None, sensitive: bool=False):
+        self.__clean_query_data()
+
+        if not self.__compare_datasets(dataset):
+            self.docs_data(dataset, sensitive)
+
+        return self.find(query, limit, umbral, sensitive)
+
+
+    def __compare_datasets(self, dataset: str):
+        """
+        Compare the documents with the set of documents
+        :param documents: list of documents
+        :get documents: set of documents
+        :return: True if the documents are the same, False if not
+        """
+
+        if self.dataset.dataset_name == dataset:
+            return True
+
+        return False
+
+
+    def docs_data(self, dataset: str, sensitive: bool= False):
+        self.dataset.get_dataset(dataset)
+        self.__get_term_docs(sensitive)
+
+    
+    def find(self, query: str, limit: int=None, umbral: float=None, sensitive: bool=False):
+        """
+        :param query: query to search
+        :param documents: list of documents
+        :get query_terms: list with query terms
+        :return: list of documents sorted by similarity
+        """
+
+        if sensitive:
+            self.__query_terms(query)
+        else:
+            self.__query_terms(query.lower())
+
+        self.__sim()
+
+        rank = self.__ranking(limit, umbral)
+        
+        return rank
+
+
+    def __clean_query_data(self):
+        """
+        Clean the query data
+        :get query_terms: empty dictionary to store query terms
+        """
+
+        self.query_terms.clear()
+
+
+    def __ranking(self, limit: int, umbral: float):
+        new_query_sim = dict()
+
+        for doc in self.query_doc_sim:
+            if self.query_doc_sim[doc] != 0:
+                new_query_sim[doc] = self.query_doc_sim[doc]
+
+        self.query_doc_sim = new_query_sim
+
+        rank = sorted(self.query_doc_sim.items(), key=lambda x: x[1], reverse=True)
+        
+        if umbral != None:
+            rank= self.__umbral(rank, umbral)
+        
+        if limit != None:
+            rank= rank[:limit]
+        
+        return rank
+
+
+    def __umbral(self, rank: list, umbral: float):
+        """
+        Filter the documents by the similarity using the umbral
+        :param rank: list of documents sorted by similarity
+        :param umbral: similarity umbral
+        :return: list of documents that pass the umbral
+        """
+        newrank= []
+
+        for doc in rank:
+            if doc[1] >= umbral:
+                newrank.append(doc)
+
+        return newrank
 
 
     def __get_term_docs(self, sensitive: bool):
@@ -62,7 +159,7 @@ class Probabilistic_model(Model):
                 self.query_terms.append(term)
 
 
-    def __sim_const(self):
+    def __sim(self):
         """
         Computes the similarity between the query and the documents from the collection
         takes as a constant value p_i = 0.5
@@ -86,3 +183,5 @@ class Probabilistic_model(Model):
                 else:                      
                     if self.query_doc_sim.get(doc) == None:                           
                         self.query_doc_sim[doc] = 0
+
+
