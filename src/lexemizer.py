ee8d5f41fcb820cfdb27985756ab86b23dba1d6a
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-06 00:36:24 -0500

    lexemizer

---
+++ src/lexemizer.py
@@ -0,0 +1,22 @@
+from os import getcwd
+from os.path import join
+import re
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+nltk.download('stopwords')
+
+#todo: use module for estopwords
+class Lexemizer:
+    def __init__(self):
+        self.stopwords:set = set(nltk.corpus.stopwords.words('english'))
+        self.lancaster = LancasterStemmer()
+
+    def not_stopwords_terms(self, terms:list):
+        new_terms= []
+        for term in terms:
+            if self.stopwords.get(term) == None:
+                new_terms.append(term)
+        return terms
+    
+    def normalize(self, text: str):
+        return [self.lancaster.stem(token.lower()) for token in re.split(r'\W+', text) if token not in self.stopwords]
