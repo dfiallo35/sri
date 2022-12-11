969a1814505da3a4a751f035cda3a3d942b56c19
dionisio35 <86890736+dionisio35@users.noreply.github.com>
2022-12-10 22:46:35 -0500

    consult expansion

--- src/lexemizer.py
+++ src/lexemizer.py
@@ -1,12 +1,25 @@
 import re
 import nltk
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet, stopwords
 
 class Lexemizer:
     def __init__(self):
         nltk.download('stopwords')
-        self.stopwords:set = set(nltk.corpus.stopwords.words('english'))
-        self.lancaster = LancasterStemmer()
+        self.stopwords:set = set(stopwords.words('english'))
+        self.lexemizer = WordNetLemmatizer()
     
-    def normalize(self, text: str):
-        return [self.lancaster.stem(token.lower()) for token in re.split(r'\W+', text) if token not in self.stopwords]
+    def normalize(self, text: str) -> list:
+        return [self.lexemizer.lemmatize(token.lower()) for token in re.split(r'\W+', text) if token not in self.stopwords]
+    
+
+    def consult_expansion(query: list) -> list:
+        newquery=[]
+        for term in query:
+            for synset in wordnet.synsets(term):
+                for lemma in synset.lemmas():
+                    newquery.append(lemma.name())
+        print('-------------------------------')
+        print(query)
+        print(list(set(newquery)))
+        return list(set(newquery))
